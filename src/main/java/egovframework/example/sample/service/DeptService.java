045aaf0e2fc7b222b43a733501739a67fef6f092
cojun <dhkdzh70@gmail.com>
2023-12-28 02:02:38 +0900

    Dept_Insert_Service_Impl_DAO_xml set

---
+++ src/main/java/egovframework/example/sample/service/DeptService.java
@@ -0,0 +1,7 @@
+package egovframework.example.sample.service;
+
+public interface DeptService {
+	
+	public String InsertDept(DeptVO vo) throws Exception;
+
+}
--- src/main/java/egovframework/example/sample/web/DeptController.java
+++ src/main/java/egovframework/example/sample/web/DeptController.java
@@ -1,12 +1,18 @@
 package egovframework.example.sample.web;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import egovframework.example.sample.service.DeptService;
 import egovframework.example.sample.service.DeptVO;
 
 @Controller
 public class DeptController {
+	
+	@Resource(name="deptService")
+	private DeptService deptService;
 	
 	@RequestMapping(value = "/deptWrite.do")
 	public String deptWrite() {
@@ -16,12 +22,20 @@ public class DeptController {
 	}
 	
 	@RequestMapping(value = "/deptWriteSave.do")
-	public String deptWriteSave(DeptVO vo) {
+	public String deptWriteSave(DeptVO vo) throws Exception {
 		
 		System.out.println("부서번호 : " + vo.getDeptno());
 		System.out.println("부서이름 : " + vo.getDname());
 		System.out.println("부서위치 : " + vo.getLoc());
-
+		
+		String result = deptService.InsertDept(vo);
+		
+		if(result == null) { // 저장 됨 
+			System.out.println("저장완료");
+		}else {
+			System.out.println("저정실패");
+		}
+		
 		
 		return "";
 	}
